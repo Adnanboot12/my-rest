25fa4784898a32ef632a30840d2a576044791e3b
Percy <percy@railway.app>
2024-03-12 15:48:17 +0000

    Initial Commit

---
+++ settings.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+creator ='Adnanwebapikey'
+MONGO_DB_URI =process.env.MONGO_DB_URI
+ACTIVATION_TOKEN_SECRET =process.env.ACTIVATION_TOKEN_SECRET 
+your_email =process.env.your_email 
+email_password = process.env.email_password
+limitCount = 99999999999
+YUOR_PORT = 8000
+loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
